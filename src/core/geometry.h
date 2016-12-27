9f487defe02f19ae24a36f99ad6b10a13355ec13
Brian Ho <brian@brkho.com>
2016-12-27 18:40:51 +0800

    Add Vector2 and Vector3 support.

--- src/core/geometry.h
+++ src/core/geometry.h
@@ -47,7 +47,61 @@ class Vector2 {
     Vector2<T> &operator+=(const Vector2<T> &that) {
       x += that.x;
       y += that.y;
+      assert(IsValid());
       return *this;
+    }
+
+    // Component-wise subtraction returning a new Vector2.
+    Vector2<T> operator-(const Vector2<T> &that) const {
+      return Vector2<T>(x - that.x, y - that.y);
+    }
+
+    // Component-wise assignment subtraction.
+    Vector2<T> &operator-=(const Vector2<T> &that) {
+      x -= that.x;
+      y -= that.y;
+      assert(IsValid());
+      return *this;
+    }
+
+    // Component-wise multiplication with a scalar returning a new Vector2.
+    Vector2<T> operator*(T scalar) const {
+      return Vector2<T>(x * scalar, y * scalar);
+    }
+
+    // Component-wise assignment multiplication with a scalar.
+    Vector2<T> &operator*=(T scalar) {
+      x *= scalar;
+      y *= scalar;
+      assert(IsValid());
+      return *this;
+    }
+
+    // Component-wise division with a scalar returning a new Vector2.
+    Vector2<T> operator/(T scalar) const {
+      assert(scalar != 0);
+      float inverse = 1.0 / scalar;
+      return Vector2<T>(x * inverse, y * inverse);
+    }
+
+    // Component-wise assignment division with a scalar.
+    Vector2<T> &operator/=(T scalar) {
+      assert(scalar != 0);
+      float inverse = 1.0 / scalar;
+      x *= inverse;
+      y *= inverse;
+      assert(IsValid());
+      return *this;
+    }
+
+    // Returns the squared length of the vector.
+    float LengthSquared() const {
+      return x * x + y * y;
+    }
+
+    // Returns the length of the vector.
+    float Length() const {
+      return std::sqrt(LengthSquared());
     }
 
     // Pretty prints a Vector2.
@@ -60,6 +114,184 @@ class Vector2 {
       return !std::isnan(x) && !std::isnan(y);
     }
 };
+
+// Returns the dot product of two Vector2s.
+template <typename T>
+inline T Dot(const Vector2<T> &vec1, const Vector2<T> &vec2) {
+  return vec1.x * vec2.x + vec1.y * vec2.y;
+}
+
+// Returns the component-wise absolute value of a Vector2.
+template <typename T>
+inline Vector2<T> Abs(const Vector2<T> &vec) {
+  return Vector2<T>(std::abs(vec.x), std::abs(vec.y));
+}
+
+// Normalizes a Vector2 so its length is 1.
+template <typename T>
+inline Vector2<T> Normalize(const Vector2<T> &vec) {
+  return vec / vec.Length();
+}
+
+// Some type declarations for common usages of Vector2.
+typedef Vector2<int> Vector2i;
+typedef Vector2<float> Vector2f;
+
+// A Vector representing three elements parameterized for reuse with both floats and integers.
+template <typename T>
+class Vector3 {
+  public:
+    // The values of the vector.
+    T x, y, z;
+
+    // Default constructor for a Vector3 that initializes x, y, and z to 0.
+    Vector3() {
+      x = 0;
+      y = 0;
+      z = 0;
+    }
+
+    // Constructor for a Vector3 that takes an x, y, and z value.
+    Vector3(T x, T y, T z) : x(x), y(y), z(z) { assert(IsValid()); }
+
+    // Accessing via integer index for reading.
+    T operator[](int i) const {
+      assert(i >= 0 && i <= 2);
+      return i == 0 ? x : (i == 1 ? y : z);
+    }
+
+    // Accessing via integer index for assignment.
+    T &operator[](int i) {
+      assert(i >= 0 && i <= 2);
+      return i == 0 ? x : (i == 1 ? y : z);
+    }
+
+    // Component-wise addition returning a new Vector3.
+    Vector3<T> operator+(const Vector3<T> &that) const {
+      return Vector3<T>(x + that.x, y + that.y, z + that.z);
+    }
+
+    // Component-wise assignment addition.
+    Vector3<T> &operator+=(const Vector3<T> &that) {
+      x += that.x;
+      y += that.y;
+      z += that.z;
+      assert(IsValid());
+      return *this;
+    }
+
+    // Component-wise subtraction returning a new Vector3.
+    Vector3<T> operator-(const Vector3<T> &that) const {
+      return Vector3<T>(x - that.x, y - that.y, z - that.z);
+    }
+
+    // Component-wise assignment subtraction.
+    Vector3<T> &operator-=(const Vector3<T> &that) {
+      x -= that.x;
+      y -= that.y;
+      z -= that.z;
+      assert(IsValid());
+      return *this;
+    }
+
+    // Component-wise multiplication with a scalar returning a new Vector3.
+    Vector3<T> operator*(T scalar) const {
+      return Vector3<T>(x * scalar, y * scalar, z * scalar);
+    }
+
+    // Component-wise assignment multiplication with a scalar.
+    Vector3<T> &operator*=(T scalar) {
+      x *= scalar;
+      y *= scalar;
+      z *= scalar;
+      assert(IsValid());
+      return *this;
+    }
+
+    // Component-wise division with a scalar returning a new Vector3.
+    Vector3<T> operator/(T scalar) const {
+      assert(scalar != 0);
+      float inverse = 1.0 / scalar;
+      return Vector3<T>(x * inverse, y * inverse, z * inverse);
+    }
+
+    // Component-wise assignment division with a scalar.
+    Vector3<T> &operator/=(T scalar) {
+      assert(scalar != 0);
+      float inverse = 1.0 / scalar;
+      x *= inverse;
+      y *= inverse;
+      z *= inverse;
+      assert(IsValid());
+      return *this;
+    }
+
+    // Returns the squared length of the vector.
+    float LengthSquared() const {
+      return x * x + y * y + z * z;
+    }
+
+    // Returns the length of the vector.
+    float Length() const {
+      return std::sqrt(LengthSquared());
+    }
+
+    // Pretty prints a Vector3.
+    std::string ToString() {
+      return "(" + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z) + ")";
+    }
+  private:
+    // Returns whether the vector is valid. This returns true if there are no NaNs, else false.
+    bool IsValid() const {
+      return !std::isnan(x) && !std::isnan(y) && !std::isnan(z);
+    }
+};
+
+// Returns the dot product of two Vector3s.
+template <typename T>
+inline T Dot(const Vector3<T> &vec1, const Vector3<T> &vec2) {
+  return vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z;
+}
+
+// Returns the cross product of two Vector3s.
+template <typename T>
+inline Vector3<T> Cross(const Vector3<T> &vec1, const Vector3<T> &vec2) {
+  // Convert to double before performing the operation to guard against floating point errors as
+  // described in pbrt.
+  double v1x = vec1.x;
+  double v1y = vec1.y;
+  double v1z = vec1.z;
+  double v2x = vec2.x;
+  double v2y = vec2.y;
+  double v2z = vec2.z;
+  return Vector3<T>((v1y * v2z) - (v1z * v2y), (v1z * v2x) - (v1x * v2z),
+      (v1x * v2y) - (v1y * v2x));
+}
+
+// Returns the component-wise absolute value of a Vector3.
+template <typename T>
+inline Vector3<T> Abs(const Vector3<T> &vec) {
+  return Vector3<T>(std::abs(vec.x), std::abs(vec.y), std::abs(vec.z));
+}
+
+// Normalizes a Vector3 so its length is 1.
+template <typename T>
+inline Vector3<T> Normalize(const Vector3<T> &vec) {
+  return vec / vec.Length();
+}
+
+// Constructs a local coordinate system given a normalized 3D vector, outputting the other two
+// orthonormal vectors through the pointer arguments.
+template <typename T>
+inline void CreateCoordSystem(const Vector3<T> &vec1, Vector3<T> *vec2, Vector3<T> *vec3) {
+  *vec2 = std::abs(vec1.x) > std::abs(vec1.y) ? Normalize(Vector3<T>(-vec1.z, 0.0, vec1.x)) :
+      Normalize(Vector3<T>(0.0, vec1.z, -vec1.y));
+  *vec3 = Cross(vec1, *vec2);
+}
+
+// Some type declarations for common usages of Vector3.
+typedef Vector3<int> Vector3i;
+typedef Vector3<float> Vector3f;
 
 }
 
--- src/tests/t_geometry.cpp
+++ src/tests/t_geometry.cpp
@@ -1,45 +1,59 @@
 #include "core/geometry.h"
 #include "tests/test.h"
 
-void Vector2IntEquals(liang::Vector2<int> vec, int x, int y) {
+void Vector2IntEquals(liang::Vector2i vec, int x, int y) {
   ASSERT_EQ(vec.x, x);
   ASSERT_EQ(vec.y, y);
 }
 
-void Vector2FloatEquals(liang::Vector2<float> vec, float x, float y) {
+void Vector2FloatEquals(liang::Vector2f vec, float x, float y) {
   ASSERT_FLOAT_EQ(vec.x, x);
   ASSERT_FLOAT_EQ(vec.y, y);
 }
 
-TEST(GeometryTest, Point2Creation) {
-  liang::Vector2<int> default_int_vector = liang::Vector2<int>();
+void Vector3IntEquals(liang::Vector3i vec, int x, int y, int z) {
+  ASSERT_EQ(vec.x, x);
+  ASSERT_EQ(vec.y, y);
+  ASSERT_EQ(vec.z, z);
+}
+
+void Vector3FloatEquals(liang::Vector3f vec, float x, float y, float z) {
+  ASSERT_FLOAT_EQ(vec.x, x);
+  ASSERT_FLOAT_EQ(vec.y, y);
+  ASSERT_FLOAT_EQ(vec.z, z);
+}
+
+TEST(GeometryTest, Vector2Creation) {
+  liang::Vector2i default_int_vector = liang::Vector2i();
   Vector2IntEquals(default_int_vector, 0, 0);
-  liang::Vector2<int> initialized_int_vector = liang::Vector2<int>(-5, 5);
+  liang::Vector2i initialized_int_vector = liang::Vector2i(-5, 5);
   Vector2IntEquals(initialized_int_vector, -5, 5);
 
-  liang::Vector2<float> default_float_vector = liang::Vector2<float>();
+  liang::Vector2f default_float_vector = liang::Vector2f();
   Vector2FloatEquals(default_float_vector, 0.0, 0.0);
-  liang::Vector2<float> initialized_float_vector = liang::Vector2<float>(-5.0, 5.0);
+  liang::Vector2f initialized_float_vector = liang::Vector2f(-5.0, 5.0);
   Vector2FloatEquals(initialized_float_vector, -5.0, 5.0);
 }
 
-TEST(GeometryTest, Point2Indexing) {
-  liang::Vector2<int> vec = liang::Vector2<int>(1, 2);
+TEST(GeometryTest, Vector2Indexing) {
+  liang::Vector2i vec = liang::Vector2i(1, 2);
   Vector2IntEquals(vec, vec[0], vec[1]);
   vec[0] = 3;
   vec[1] = 4;
   Vector2IntEquals(vec, 3, 4);
 }
 
-TEST(GeometryTest, Point2IndexingOutOfRange) {
-  liang::Vector2<int> vec = liang::Vector2<int>(1, 2);
-  ASSERT_DEATH(vec[3], liang::ASSERTION_FAILURE);
+TEST(GeometryTest, Vector2IndexingOutOfRange) {
+  liang::Vector2i vec = liang::Vector2i(1, 2);
+  ASSERT_DEATH(vec[2], liang::ASSERTION_FAILURE);
   ASSERT_DEATH(vec[-1], liang::ASSERTION_FAILURE);
+  ASSERT_DEATH({ vec[2] = 5; }, liang::ASSERTION_FAILURE);
+  ASSERT_DEATH({ vec[-1] = 5; }, liang::ASSERTION_FAILURE);
 }
 
-TEST(GeometryTest, Point2Addition) {
-  liang::Vector2<float> vec1 = liang::Vector2<float>(1.1, 2.2);
-  liang::Vector2<float> vec2 = liang::Vector2<float>(0.5, 0.6);
+TEST(GeometryTest, Vector2Addition) {
+  liang::Vector2f vec1 = liang::Vector2f(1.1, 2.2);
+  liang::Vector2f vec2 = liang::Vector2f(0.5, 0.6);
   auto vec3 = vec1 + vec2;
   Vector2FloatEquals(vec1, 1.1, 2.2);
   Vector2FloatEquals(vec2, 0.5, 0.6);
@@ -48,4 +62,204 @@ TEST(GeometryTest, Point2Addition) {
   vec1 += vec2;
   Vector2FloatEquals(vec1, 1.6, 2.8);
   Vector2FloatEquals(vec2, 0.5, 0.6);
+}
+
+TEST(GeometryTest, Vector2Subtraction) {
+  liang::Vector2f vec1 = liang::Vector2f(1.6, 2.8);
+  liang::Vector2f vec2 = liang::Vector2f(0.3, 0.4);
+  auto vec3 = vec1 - vec2;
+  Vector2FloatEquals(vec1, 1.6, 2.8);
+  Vector2FloatEquals(vec2, 0.3, 0.4);
+  Vector2FloatEquals(vec3, 1.3, 2.4);
+
+  vec1 -= vec2;
+  Vector2FloatEquals(vec1, 1.3, 2.4);
+  Vector2FloatEquals(vec2, 0.3, 0.4);
+}
+
+TEST(GeometryTest, Vector2Multiplication) {
+  liang::Vector2f vec1 = liang::Vector2f(1.1, 2.1);
+  auto vec2 = vec1 * 2.0;
+  Vector2FloatEquals(vec1, 1.1, 2.1);
+  Vector2FloatEquals(vec2, 2.2, 4.2);
+
+  vec1 *= 10.0;
+  Vector2FloatEquals(vec1, 11.0, 21.0);
+}
+
+TEST(GeometryTest, Vector2Division) {
+  liang::Vector2f vec1 = liang::Vector2f(10.5, 20.5);
+  auto vec2 = vec1 / 5.0;
+  Vector2FloatEquals(vec1, 10.5, 20.5);
+  Vector2FloatEquals(vec2, 2.1,  4.1);
+
+  vec1 /= 10.0;
+  Vector2FloatEquals(vec1, 1.05, 2.05);
+}
+
+TEST(GeometryTest, Vector2DivisonByZero) {
+  liang::Vector2f vec = liang::Vector2f(1.0, 2.0);
+  ASSERT_DEATH(vec / 0.0, liang::ASSERTION_FAILURE);
+  ASSERT_DEATH({ vec /= 0.0; }, liang::ASSERTION_FAILURE);
+}
+
+TEST(GeometryTest, Vector2Abs) {
+  liang::Vector2f vec = liang::Vector2f(-1.0, -2.0);
+  Vector2FloatEquals(liang::Abs(vec), 1.0, 2.0);
+}
+
+TEST(GeometryTest, Vector2Dot) {
+  liang::Vector2f vec1 = liang::Vector2f(3.2, -5.2);
+  liang::Vector2f vec2 = liang::Vector2f(1.6, 123.4);
+  float dot = liang::Dot(vec1, vec2);
+  float dot_reversed = liang::Dot(vec2, vec1);
+  Vector2FloatEquals(vec1, 3.2, -5.2);
+  Vector2FloatEquals(vec2, 1.6, 123.4);
+  ASSERT_FLOAT_EQ(-636.56, dot);
+  ASSERT_FLOAT_EQ(-636.56, dot_reversed);
+}
+
+TEST(GeometryTest, Vector2Length) {
+  liang::Vector2f vec = liang::Vector2f(3.0, 4.0);
+  ASSERT_FLOAT_EQ(25.0, vec.LengthSquared());
+  ASSERT_FLOAT_EQ(5.0, vec.Length());
+}
+
+TEST(GeometryTest, Vector2Normalize) {
+  liang::Vector2f vec = liang::Vector2f(3.0, 4.0);
+  liang::Vector2f normalized = liang::Normalize(vec);
+  Vector2FloatEquals(vec, 3.0, 4.0);
+  Vector2FloatEquals(normalized, 0.6, 0.8);
+  ASSERT_FLOAT_EQ(1.0, normalized.Length());
+  ASSERT_FLOAT_EQ(1.0, normalized.LengthSquared());
+}
+
+TEST(GeometryTest, Vector3Creation) {
+  liang::Vector3i default_int_vector = liang::Vector3i();
+  Vector3IntEquals(default_int_vector, 0, 0, 0);
+  liang::Vector3i initialized_int_vector = liang::Vector3i(-5, 5, -10);
+  Vector3IntEquals(initialized_int_vector, -5, 5, -10);
+
+  liang::Vector3f default_float_vector = liang::Vector3f();
+  Vector3FloatEquals(default_float_vector, 0.0, 0.0, 0.0);
+  liang::Vector3f initialized_float_vector = liang::Vector3f(-5.0, 5.0, -10.0);
+  Vector3FloatEquals(initialized_float_vector, -5.0, 5.0, -10.0);
+}
+
+TEST(GeometryTest, Vector3Indexing) {
+  liang::Vector3i vec = liang::Vector3i(1, 2, 3);
+  Vector3IntEquals(vec, vec[0], vec[1], vec[2]);
+  vec[0] = 4;
+  vec[1] = 5;
+  vec[2] = 6;
+  Vector3IntEquals(vec, 4, 5, 6);
+}
+
+TEST(GeometryTest, Vector3IndexingOutOfRange) {
+  liang::Vector3i vec = liang::Vector3i(1, 2, 3);
+  ASSERT_DEATH(vec[3], liang::ASSERTION_FAILURE);
+  ASSERT_DEATH(vec[-1], liang::ASSERTION_FAILURE);
+  ASSERT_DEATH({ vec[3] = 5; }, liang::ASSERTION_FAILURE);
+  ASSERT_DEATH({ vec[-1] = 5; }, liang::ASSERTION_FAILURE);
+}
+
+TEST(GeometryTest, Vector3Addition) {
+  liang::Vector3f vec1 = liang::Vector3f(1.1, 2.2, 3.3);
+  liang::Vector3f vec2 = liang::Vector3f(0.5, 0.6, 0.7);
+  auto vec3 = vec1 + vec2;
+  Vector3FloatEquals(vec1, 1.1, 2.2, 3.3);
+  Vector3FloatEquals(vec2, 0.5, 0.6, 0.7);
+  Vector3FloatEquals(vec3, 1.6, 2.8, 4.0);
+
+  vec1 += vec2;
+  Vector3FloatEquals(vec1, 1.6, 2.8, 4.0);
+  Vector3FloatEquals(vec2, 0.5, 0.6, 0.7);
+}
+
+TEST(GeometryTest, Vector3Subtraction) {
+  liang::Vector3f vec1 = liang::Vector3f(1.6, 2.8, 5.6);
+  liang::Vector3f vec2 = liang::Vector3f(0.3, 0.4, 0.5);
+  auto vec3 = vec1 - vec2;
+  Vector3FloatEquals(vec1, 1.6, 2.8, 5.6);
+  Vector3FloatEquals(vec2, 0.3, 0.4, 0.5);
+  Vector3FloatEquals(vec3, 1.3, 2.4, 5.1);
+
+  vec1 -= vec2;
+  Vector3FloatEquals(vec1, 1.3, 2.4, 5.1);
+  Vector3FloatEquals(vec2, 0.3, 0.4, 0.5);
+}
+
+TEST(GeometryTest, Vector3Multiplication) {
+  liang::Vector3f vec1 = liang::Vector3f(1.1, 2.1, 3.1);
+  auto vec2 = vec1 * 2.0;
+  Vector3FloatEquals(vec1, 1.1, 2.1, 3.1);
+  Vector3FloatEquals(vec2, 2.2, 4.2, 6.2);
+
+  vec1 *= 10.0;
+  Vector3FloatEquals(vec1, 11.0, 21.0, 31.0);
+}
+
+TEST(GeometryTest, Vector3Division) {
+  liang::Vector3f vec1 = liang::Vector3f(10.5, 20.5, 30.5);
+  auto vec2 = vec1 / 5.0;
+  Vector3FloatEquals(vec1, 10.5, 20.5, 30.5);
+  Vector3FloatEquals(vec2, 2.1,  4.1, 6.1);
+
+  vec1 /= 10.0;
+  Vector3FloatEquals(vec1, 1.05, 2.05, 3.05);
+}
+
+TEST(GeometryTest, Vector3DivisonByZero) {
+  liang::Vector3f vec = liang::Vector3f(1.0, 2.0, 3.0);
+  ASSERT_DEATH(vec / 0.0, liang::ASSERTION_FAILURE);
+  ASSERT_DEATH({ vec /= 0.0; }, liang::ASSERTION_FAILURE);
+}
+
+TEST(GeometryTest, Vector3Abs) {
+  liang::Vector3f vec = liang::Vector3f(-1.0, -2.0, -3.0);
+  Vector3FloatEquals(liang::Abs(vec), 1.0, 2.0, 3.0);
+}
+
+TEST(GeometryTest, Vector3Dot) {
+  liang::Vector3f vec1 = liang::Vector3f(3.2, -5.2, 3.2);
+  liang::Vector3f vec2 = liang::Vector3f(1.6, 123.4, -12.1);
+  float dot = liang::Dot(vec1, vec2);
+  float dot_reversed = liang::Dot(vec2, vec1);
+  Vector3FloatEquals(vec1, 3.2, -5.2, 3.2);
+  Vector3FloatEquals(vec2, 1.6, 123.4, -12.1);
+  ASSERT_FLOAT_EQ(-675.28, dot);
+  ASSERT_FLOAT_EQ(-675.28, dot_reversed);
+}
+
+TEST(GeometryTest, Vector3Length) {
+  liang::Vector3f vec = liang::Vector3f(1.0, 2.0, 3.0);
+  ASSERT_FLOAT_EQ(14.0, vec.LengthSquared());
+  ASSERT_FLOAT_EQ(3.74165738, vec.Length());
+}
+
+TEST(GeometryTest, Vector3Normalize) {
+  liang::Vector3f vec = liang::Vector3f(1.0, 2.0, 3.0);
+  liang::Vector3f normalized = liang::Normalize(vec);
+  Vector3FloatEquals(vec, 1.0, 2.0, 3.0);
+  Vector3FloatEquals(normalized, 0.2672612, 0.53452248, 0.80178372);
+  ASSERT_FLOAT_EQ(1.0, normalized.Length());
+  ASSERT_FLOAT_EQ(1.0, normalized.LengthSquared());
+}
+
+TEST(GeometryTest, Vector3Cross) {
+  liang::Vector3f vec1 = liang::Vector3f(3.0, 4.0, 5.0);
+  liang::Vector3f vec2 = liang::Vector3f(0.1, -5.0, 10.0);
+  Vector3FloatEquals(vec1, 3.0, 4.0, 5.0);
+  Vector3FloatEquals(vec2, 0.1, -5.0, 10.0);
+  Vector3FloatEquals(liang::Cross(vec1, vec2), 65.0, -29.5, -15.4);
+}
+
+TEST(GeometryTest, Vector3CreateCoordSystem) {
+  liang::Vector3f vec1 = liang::Vector3f(0.6, 0.8, 0.0);
+  liang::Vector3f vec2 = liang::Vector3f();
+  liang::Vector3f vec3 = liang::Vector3f();
+  liang::CreateCoordSystem(vec1, &vec2, &vec3);
+  Vector3FloatEquals(vec1, 0.6, 0.8, 0.0);
+  Vector3FloatEquals(vec2, 0.0, 0.0, -1.0);
+  Vector3FloatEquals(vec3, -0.8, 0.6, 0.0);
 }
